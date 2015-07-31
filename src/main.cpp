62fe731ac735375e116bf3edd5f1ce29c9849618
duhone <duhone@conjuredrealms.com>
2015-07-30 22:50:47 -0700

    comment out test code. now launching from server

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,14 @@
 
 int _tmain(int /*argc*/, _TCHAR* /*argv[]*/)
 {
-	auto core = LoadCore("cores/mame_libretro.dll");
+	/*auto core = LoadCore("cores/mame_libretro.dll");
 	auto romPath = CR::Platform::RelativeToAbsolute("roms/galaga.zip");
 	core->LoadGame(romPath.c_str());
 
 	for(int i = 0; i < 200; ++i)
 	{
 		core->RunOneFrame();
-	}
+	}*/
 
 	return 0;
 }
